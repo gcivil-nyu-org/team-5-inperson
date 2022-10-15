1219c5892ff042e887630a2bde27f993f429b5c1
Suyash Soniminde <67739920+TheHarbinger99@users.noreply.github.com>
2022-10-15 10:43:05 -0400

    Update urls.py
    Latitude and Longitude swap

--- backend/NycBasics/urls.py
+++ backend/NycBasics/urls.py
@@ -3,9 +3,9 @@ from . import converters, views
 register_converter(converters.FloatUrlParameterConverter, 'float')
 
 urlpatterns = [
-    path('api/water/<float:pk1>/<float:pk2>/', views.water_List.as_view() ),
-    path('api/wifi/<float:pk1>/<float:pk2>/', views.wifi_List.as_view() ),
-    path('api/bench/<float:pk1>/<float:pk2>/', views.bench_List.as_view() ),
-    path('api/toilet/<float:pk1>/<float:pk2>/', views.toilet_List.as_view() ),
-    path('api/parking/<float:pk1>/<float:pk2>/', views.parking_List.as_view() ),        
+    path('api/water/<float:pk2>/<float:pk1>/', views.water_List.as_view() ),
+    path('api/wifi/<float:pk2>/<float:pk1>/', views.wifi_List.as_view() ),
+    path('api/bench/<float:pk2>/<float:pk1>/', views.bench_List.as_view() ),
+    path('api/toilet/<float:pk2>/<float:pk1>/', views.toilet_List.as_view() ),
+    path('api/parking/<float:pk2>/<float:pk1>/', views.parking_List.as_view() ),        
 ]
