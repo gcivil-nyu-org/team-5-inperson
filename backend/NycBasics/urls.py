85b5be90d11a7149a61d71122b5eae557e48016d
TheHarbinger99 <sonimindesuyash99@gmail.com>
2022-10-15 06:59:41 -0400

    Updated APi's and backend to give nearyby locations to mapcenter

--- backend/NycBasics/urls.py
+++ backend/NycBasics/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
-from . import views
+from django.urls import path, register_converter
+from . import converters, views
+register_converter(converters.FloatUrlParameterConverter, 'float')
 
 urlpatterns = [
-    path('api/water/', views.water_List.as_view() ),
-    path('api/wifi/', views.wifi_List.as_view() ),
-    path('api/bench/', views.bench_List.as_view() ),
-    path('api/toilet/', views.toilet_List.as_view() ),
-    path('api/parking/', views.parking_List.as_view() ),        
+    path('api/water/<float:pk1>/<float:pk2>/', views.water_List.as_view() ),
+    path('api/wifi/<float:pk1>/<float:pk2>/', views.wifi_List.as_view() ),
+    path('api/bench/<float:pk1>/<float:pk2>/', views.bench_List.as_view() ),
+    path('api/toilet/<float:pk1>/<float:pk2>/', views.toilet_List.as_view() ),
+    path('api/parking/<float:pk1>/<float:pk2>/', views.parking_List.as_view() ),        
 ]
