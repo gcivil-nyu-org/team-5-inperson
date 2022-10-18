1d222e8e136d10e264927e703f8e382244d2ad01
TheHarbinger99 <sonimindesuyash99@gmail.com>
2022-10-18 19:36:51 -0400

    integration update for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 #'/home/django/django_project/django_project/static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static/')
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 """
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static'),
