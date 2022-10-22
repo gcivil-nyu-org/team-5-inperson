15d7aff6544720e68f714fde1fe3b0081b5f534f
TheHarbinger99 <sonimindesuyash99@gmail.com>
2022-10-22 16:02:19 -0400

    Created Api's for amenity details

--- NycBasics/urls.py
+++ NycBasics/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path("api/bench/<float:pk2>/<float:pk1>/", views.bench_List.as_view()),
     path("api/toilet/<float:pk2>/<float:pk1>/", views.toilet_List.as_view()),
     path("api/parking/<float:pk2>/<float:pk1>/", views.parking_List.as_view()),
+
+    path("api/water/<int:pk>/", views.water_amenity_detail.as_view()),
+    path("api/wifi/<int:pk>/", views.wifi_amenity_detail.as_view()),
+    path("api/bench/<int:pk>/", views.bench_amenity_detail.as_view()),
+    path("api/toilet/<int:pk>/", views.toilet_amenity_detail.as_view()),
+    path("api/parking/<int:pk>/", views.parking_amenity_detail.as_view()),
 ]
