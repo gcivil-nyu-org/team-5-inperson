126e263c0ba142d33df146c61f649f721aa25111
Viha Gupta <vihagupta95@gmail.com>
2022-10-23 22:34:31 -0400

    getting 92% coverage

--- backend/frontend/src/App.test.js
+++ backend/frontend/src/App.test.js
@@ -50,25 +50,46 @@ test('all bench markers are shown and then hidden when we toggle the bench filte
   expect(benchMarkers.length).toEqual(0);
 });
 
-// test('all toilet markers are shown and then hidden when we toggle the toilet filter', async () => {
-//   render(<App />);
-//   const toiletFilterBtn = screen.getByTestId('toilet-filter-btn');
-//   userEvent.click(toiletFilterBtn);
-//   let toiletMarkers = await screen.findAllByTestId('toilet-marker');
-//   expect(toiletMarkers.length).toEqual(toiletMock.length);
-//   userEvent.click(toiletFilterBtn);
-//   toiletMarkers = screen.queryAllByTestId('toilet-marker');
-//   expect(toiletMarkers.length).toEqual(0);
-// });
+test('all toilet markers are shown and then hidden when we toggle the toilet filter', async () => {
+  render(<App />);
+  const toiletFilterBtn = screen.getByTestId('toilet-filter-btn');
+  userEvent.click(toiletFilterBtn);
+  let toiletMarkers = await screen.findAllByTestId('toilet-marker');
+  expect(toiletMarkers.length).toEqual(toiletMock.length);
+  userEvent.click(toiletFilterBtn);
+  toiletMarkers = screen.queryAllByTestId('toilet-marker');
+  expect(toiletMarkers.length).toEqual(0);
+});
 
-// test('all wifi markers are shown and then hidden when we toggle the wifi filter', async () => {
-//   render(<App />);
-//   const wifiFilterBtn = screen.getByTestId('wifi-filter-btn');
-//   userEvent.click(wifiFilterBtn);
-//   screen.debug()
-//   let wifiMarkers = await screen.findAllByTestId('wifi-marker');
-//   expect(wifiMarkers.length).toEqual(wifiMock.length);
-//   userEvent.click(wifiFilterBtn);
-//   wifiMarkers = screen.queryAllByTestId('wifi-marker');
-//   expect(wifiMarkers.length).toEqual(0);
-// });
+test('all wifi markers are shown and then hidden when we toggle the wifi filter', async () => {
+  render(<App />);
+  const wifiFilterBtn = screen.getByTestId('wifi-filter-btn');
+  userEvent.click(wifiFilterBtn);
+  let wifiMarkers = await screen.findAllByTestId('wifi-marker');
+  expect(wifiMarkers.length).toEqual(wifiMock.length);
+  userEvent.click(wifiFilterBtn);
+  wifiMarkers = screen.queryAllByTestId('wifi-marker');
+  expect(wifiMarkers.length).toEqual(0);
+});
+
+test('all parking markers are shown and then hidden when we toggle the parking filter', async () => {
+  render(<App />);
+  const parkingFilterBtn = screen.getByTestId('parking-filter-btn');
+  userEvent.click(parkingFilterBtn);
+  let parkingMarkers = await screen.findAllByTestId('parking-marker');
+  expect(parkingMarkers.length).toEqual(parkingMock.length);
+  userEvent.click(parkingFilterBtn);
+  parkingMarkers = screen.queryAllByTestId('parking-marker');
+  expect(parkingMarkers.length).toEqual(0);
+});
+
+test('all water markers are shown and then hidden when we toggle the water filter', async () => {
+  render(<App />);
+  const waterFilterBtn = screen.getByTestId('water-filter-btn');
+  userEvent.click(waterFilterBtn);
+  let waterMarkers = await screen.findAllByTestId('water-marker');
+  expect(waterMarkers.length).toEqual(waterMock.length);
+  userEvent.click(waterFilterBtn);
+  waterMarkers = screen.queryAllByTestId('water-marker');
+  expect(waterMarkers.length).toEqual(0);
+});
