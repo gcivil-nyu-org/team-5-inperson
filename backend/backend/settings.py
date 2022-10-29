7663e0f71cef38d49c366db3541bbeedbac841cb
TheHarbinger99 <sonimindesuyash99@gmail.com>
2022-10-29 17:01:16 -0400

    rds configured

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = [
     "NYCbasics-staging.eba-itqvcpc2.us-west-2.elasticbeanstalk.com",
     "NYCbasics-prod.eba-itqvcpc2.us-west-2.elasticbeanstalk.com",
     "nycbasics5prod.ml",
+    "rds-Suyash-env.eba-x9nyundr.us-west-2.elasticbeanstalk.com",
 ]
 # add aws cname here after green eb status
 
@@ -85,11 +87,23 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": str(BASE_DIR / "db.sqlite3"),
+    }
+}
+"""
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("dbname"),
+        "USER": config("dbuser"),
+        "PASSWORD": config("dbpassword"),
+        "HOST": config("dbhost"),
+        "PORT": config("dbport"),
     }
 }
 
@@ -157,5 +171,7 @@ CORS_ORIGIN_WHITELIST = [
     "https://NYCbasics-prod.eba-itqvcpc2.us-west-2.elasticbeanstalk.com",
     "http://nycbasics5prod.ml",
     "https://nycbasics5prod.ml",
+    "http://rds-Suyash-env.eba-x9nyundr.us-west-2.elasticbeanstalk.com",
+    "https://rds-Suyash-env.eba-x9nyundr.us-west-2.elasticbeanstalk.com",
 ]
 # may need to add aws eb cname here above
