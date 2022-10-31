f5b526af6d251f8a0fe294b96bbeb689a706d44e
Adnan Ahsan <aa5973@nyu.edu>
2022-10-31 16:24:43 -0400

    Fixed CSS and added Password Validation

--- backend/frontend/src/components/Loginform.jsx
+++ backend/frontend/src/components/Loginform.jsx
@@ -1,3 +1,4 @@
+import '../styles/Form.css';
 import React, { useState } from 'react'
 import { ApiService } from '../api-service';
 import { useNavigate, Link } from 'react-router-dom';
--- backend/frontend/src/components/Signup.jsx
+++ backend/frontend/src/components/Signup.jsx
@@ -1,12 +1,12 @@
 import '../styles/Form.css';
 import React, { useState } from 'react';
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 import { ApiService } from '../api-service';
 import { useNavigate } from 'react-router-dom';
 
 function Signup() {
 
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { register, handleSubmit, getValues, watch, formState: { errors } } = useForm()
   const navigate = useNavigate();
   const [usernameError, setUsernameError] = useState("");
   const [emailError, setEmailError] = useState("");
@@ -14,6 +14,8 @@ function Signup() {
   const onSubmit = async (data) => {
     console.log("signup data", data);
     const apiService = new ApiService();
+  
+  console.log(watch("password"));
 
     try {
       const signupResponse = await apiService.addUser(data);
@@ -38,83 +40,30 @@ function Signup() {
 
   return (
     <section>
-      <div className="register">
-        <div className="col-1"></div>
-        <br></br>
-        <h2>Signup Form</h2>
-        <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-          <br></br>
-          <input type='text' {...register("username", { required: true })} placeholder='username' />
-          {errors.username?.type === "required" && "Username is Required"}
-          {(usernameError !== "") ? (<div className="error">{usernameError}</div>) : ""}
-          <br></br>
-          <input type='text' {...register("password", { required: true })} placeholder='password' />
-          {errors.password?.type === "required" && "Password is Required"}
-          <br></br>
-          <input type='text' {...register("confirmpwd", { required: true })} placeholder='confirm password' />
-          {errors.confirmpwd?.type === "required" && "Please Confirm your Password"}
-          <br></br>
-          <input type='text' {...register("email", { required: true })} placeholder='email' />
+      <div className="login">
+        <div className="form-container"></div>
+        <form id='form' className='form-inner' onSubmit={handleSubmit(onSubmit)}>
+          <h2>Signup Form</h2>
+          <label htmlFor="email">Email: </label>
+          <input type='text' {...register("email", { required: true })} placeholder='' />
           {errors.email?.type === "required" && "Email is Required"}
           {(emailError !== "") ? (<div className="error">{emailError}</div>) : ""}
-          <br></br>
+          <label htmlFor="password">Password: </label>
+          <input type='password' {...register("password", { required: true })} placeholder='' />
+          {errors.password?.type === "required" && "Password is Required"}
+          <label htmlFor="confirmpassword">Confirm Password: </label>
+          <input type='password' {...register("confirmpwd", { required: true })} placeholder='' />
+          {errors.confirmpwd?.type === "required" && "Please Confirm your Password"}
+          {watch("confirmpwd") !== watch("password") &&
+          getValues("confirmpwd") ? (
+            <p>password not match</p>
+          ) : null}
           <br></br>
           <button className='btn'> Signup </button>
         </form>
-
-        <div className="col-2"></div>
       </div>
     </section>
   )
-
-  /*
-      const {
-          register,
-          handleSubmit,
-          watch,
-          formState: { errors }
-        } = useForm();
-      
-        const onSubmit = (data) => {
-          alert(JSON.stringify(data));
-        }; // your form submit function which will invoke after successful validation
-      
-        console.log(watch("example")); // you can watch individual input by pass the name of the input
-  
-        return (
-  
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <label>Name</label>
-            <input
-              {...register("Name", {
-                required: true,
-                maxLength: 30
-              })}
-            />
-            {errors?.name?.type === "required" && <p>This field is required</p>}
-            {errors?.name?.type === "maxLength" && (
-              <p>name cannot exceed 30 characters</p>
-            )}
-            {errors?.name?.type === "pattern" && (
-              <p>No inappropriate words</p>
-            )}
-            <label>Password</label>
-            <input {...register("Password", { 
-                required: true,
-                maxLength: 30
-                })} />
-            {errors?.password?.type === "required" && <p>This field is required</p>}
-            <label>Email</label>
-            <input {...register("Email", { 
-              required: true,
-              maxLength: 30
-              })} />
-            {errors?.Email?.type === "required" && <p>This field is required</p>}
-            <input type="submit" />
-          </form>
-  
-        );
-  */
 }
 
 export default Signup
