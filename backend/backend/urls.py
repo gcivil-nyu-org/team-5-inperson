034c99bd306fd08aff171609c7fd48403c6d8b16
TheHarbinger99 <sonimindesuyash99@gmail.com>
2022-10-31 23:24:44 -0400

    updated backend urls to match frontend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,6 +22,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("NycBasics/", include("NycBasics.urls")),
+    path("home/NycBasics/", include("NycBasics.urls")),
     path("", TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
