f8f27f0c5f163a8de56641ac2c6c51527a26663f
Viha Gupta <vihagupta95@gmail.com>
2022-11-07 11:47:23 -0500

    black

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("NycBasics/", include("NycBasics.urls")),
     path("", TemplateView.as_view(template_name="index.html")),
     path("home", TemplateView.as_view(template_name="index.html")),
-    path("login", TemplateView.as_view(template_name="index.html"))
+    path("login", TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
