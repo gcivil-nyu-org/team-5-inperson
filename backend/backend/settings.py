d0feb9b0e68f3fec409830e800592389966e1be1
Viha Gupta <vihagupta95@gmail.com>
2022-11-29 14:12:43 -0500

    change sqlite to rds in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,25 +107,25 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": str(BASE_DIR / "db.sqlite3"),
-    }
-}
-
-
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("dbname"),
-#         "USER": config("dbuser"),
-#         "PASSWORD": config("dbpassword"),
-#         "HOST": config("dbhost"),
-#         "PORT": config("dbport"),
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": str(BASE_DIR / "db.sqlite3"),
 #     }
 # }
+
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("dbname"),
+        "USER": config("dbuser"),
+        "PASSWORD": config("dbpassword"),
+        "HOST": config("dbhost"),
+        "PORT": config("dbport"),
+    }
+}
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
