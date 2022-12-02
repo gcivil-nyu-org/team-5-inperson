b698fb9c58d5f1c68673d4cecedd3cf48823b011
dineshsk98 <90722119+ds6332@users.noreply.github.com>
2022-12-02 17:28:36 -0500

    formatting

--- backend/NycBasics/urls.py
+++ backend/NycBasics/urls.py
@@ -65,11 +65,7 @@ urlpatterns = [
         userviews.Login.as_view(),
         name="login",
     ),
-    path(
-        "api/logout/", 
-        userviews.Logout.as_view(), 
-        name="logout"
-    ),
+    path("api/logout/", userviews.Logout.as_view(), name="logout"),
     path(
         "api/rating_review/<str:pk1>/<int:pk2>/",
         ratingviews.rating_List.as_view(),
